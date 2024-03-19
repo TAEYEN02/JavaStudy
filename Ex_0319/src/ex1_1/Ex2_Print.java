1b940a8ec7389c54b91182e7cd42eb43a745427d
TAEYEN02 <107397675+TAEYEN02@users.noreply.github.com>
2024-03-19 21:52:34 +0900

    Add files via upload

---
+++ Ex_0319/src/ex1_1/Ex2_Print.java
@@ -0,0 +1,16 @@
+package ex1_1;
+
+public class Ex2_Print {
+	public static void main(String[] args) {
+		System.out.print("Welcome");
+		System.out.print("Java World");
+		// print: 출력하고 개행이 안됨
+		
+		System.out.println("Welcome");
+		System.out.println("Java World");
+		//println: 출력하고 개행이 됨(줄바꿈)
+		
+		
+	}
+
+}
